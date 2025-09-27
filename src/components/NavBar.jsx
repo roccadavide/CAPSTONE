39cfb4eb4ab9cad6f5bad5d5c90c385a79025e0e
roccadavide <davide.rocca03@gmail.com>
2025-09-27 11:42:13 +0200

    27/09 first commit -navbar fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { Container, NavDropdown } from "react-bootstrap";
-import { BagHeart, PersonCircle } from "react-bootstrap-icons";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { PersonCircle } from "react-bootstrap-icons";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../redux/action/authActions";
@@ -8,6 +8,7 @@ import CartIcon from "./CartIcon";
 
 const NavBar = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [expanded, setExpanded] = useState(false);
   const { user } = useSelector(state => state.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -18,90 +19,130 @@ const NavBar = () => {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
+  const closeMenu = () => setExpanded(false);
+
   return (
-    <nav className={`navbar navbar-expand-lg fixed-top ${scrolled ? "navbar-solid" : "navbar-gradient"}`}>
-      <Container fluid>
-        <Link className="navbar-brand" to="/">
+    <Navbar expand="md" fixed="top" expanded={expanded} onToggle={setExpanded} className={scrolled ? "navbar-solid" : "navbar-gradient"}>
+      <Container fluid className="px-3 position-relative d-flex align-items-center container-navbar">
+        <Navbar.Brand as={Link} to="/" onClick={closeMenu} className="d-flex align-items-center">
           <img src="/BEAUTY ROOM IMMAGINE.png" className="logo" alt="Logo" />
-        </Link>
+        </Navbar.Brand>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <Navbar.Collapse id="mainNav" className="mobile-overlay">
+          <Nav className="mx-md-auto my-3 my-md-0 gap-md-4">
+            <Nav.Link as={Link} to="/prodotti" className="nav-link-animated text-center" onClick={closeMenu}>
+              Prodotti
+            </Nav.Link>
+            <Nav.Link as={Link} to="/trattamenti" className="nav-link-animated text-center" onClick={closeMenu}>
+              Trattamenti
+            </Nav.Link>
+            <Nav.Link as={Link} to="/risultati" className="nav-link-animated text-center" onClick={closeMenu}>
+              Risultati
+            </Nav.Link>
+            <Nav.Link as={Link} to="/chisono" className="nav-link-animated text-center" onClick={closeMenu}>
+              Chi sono
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
 
-        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-          <ul className="navbar-nav gap-5">
-            <li className="nav-item">
-              <Link className="nav-link text-black" to="/prodotti">
-                Prodotti
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-black" to="/trattamenti">
-                Trattamenti
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-black" to="/risultati">
-                Risultati
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-black" to="/chisono">
-                Chi sono
-              </Link>
-            </li>
-          </ul>
+        <div className="navbar-center-toggle d-md-none">
+          <Navbar.Toggle aria-controls="mainNav" className="shadow-none">
+            <span className="navbar-toggler-icon" />
+          </Navbar.Toggle>
         </div>
-        <div className="d-flex align-items-center gap-5 me-5">
+
+        <div className="nav-right d-flex align-items-center gap-4 ms-auto" style={{ zIndex: 1040 }}>
           {user ? (
-            user.role === "ADMIN" ? (
-              <>
-                <NavDropdown title={<PersonCircle size={24} color="black" />} id="navbarScrollingDropdown">
-                  <NavDropdown.Header className="text-black" style={{ fontSize: "1.5rem" }}>
-                    Ciao, {user.name}!
-                  </NavDropdown.Header>
-                  <NavDropdown.Item onClick={() => navigate("/mioprofilo")}>Il mio profilo</NavDropdown.Item>
-                  <NavDropdown.Item onClick={() => navigate("/prenotazioni/tutte")}>Visualizza prenotazioni</NavDropdown.Item>
-                  <NavDropdown.Item onClick={() => navigate("/ordini/tutti")}>Visualizza ordini</NavDropdown.Item>
-                  <NavDropdown.Item onClick={() => dispatch(logout())}>Logout</NavDropdown.Item>
-                </NavDropdown>
-                <CartIcon />
-              </>
-            ) : (
-              <>
-                <NavDropdown title={<PersonCircle size={24} color="black" />} id="navbarScrollingDropdown">
-                  <NavDropdown.Header className="text-black" style={{ fontSize: "1.5rem" }}>
-                    Ciao, {user.name}!
-                  </NavDropdown.Header>
-                  <NavDropdown.Item onClick={() => navigate("/mioprofilo")}>Il mio profilo</NavDropdown.Item>
-                  <NavDropdown.Item onClick={() => navigate("/prenotazioni")}>Le mie prenotazioni</NavDropdown.Item>
-                  <NavDropdown.Item onClick={() => navigate("/ordini")}>I miei ordini</NavDropdown.Item>
-                  <NavDropdown.Item onClick={() => dispatch(logout())}>Logout</NavDropdown.Item>
-                </NavDropdown>
-                <CartIcon />
-              </>
-            )
+            <>
+              <NavDropdown align="end" title={<PersonCircle size={24} color="black" />} id="navbar-profile-dropdown">
+                <NavDropdown.Header className="text-black" style={{ fontSize: "1.05rem" }}>
+                  Ciao, {user.name}!
+                </NavDropdown.Header>
+
+                {user.role === "ADMIN" ? (
+                  <>
+                    <NavDropdown.Item
+                      onClick={() => {
+                        navigate("/mioprofilo");
+                        closeMenu();
+                      }}
+                    >
+                      Il mio profilo
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      onClick={() => {
+                        navigate("/prenotazioni/tutte");
+                        closeMenu();
+                      }}
+                    >
+                      Visualizza prenotazioni
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      onClick={() => {
+                        navigate("/ordini/tutti");
+                        closeMenu();
+                      }}
+                    >
+                      Visualizza ordini
+                    </NavDropdown.Item>
+                  </>
+                ) : (
+                  <>
+                    <NavDropdown.Item
+                      onClick={() => {
+                        navigate("/mioprofilo");
+                        closeMenu();
+                      }}
+                    >
+                      Il mio profilo
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      onClick={() => {
+                        navigate("/prenotazioni");
+                        closeMenu();
+                      }}
+                    >
+                      Le mie prenotazioni
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      onClick={() => {
+                        navigate("/ordini");
+                        closeMenu();
+                      }}
+                    >
+                      I miei ordini
+                    </NavDropdown.Item>
+                  </>
+                )}
+
+                <NavDropdown.Divider />
+                <NavDropdown.Item
+                  onClick={() => {
+                    dispatch(logout());
+                    closeMenu();
+                  }}
+                >
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+              <CartIcon />
+            </>
           ) : (
             <>
-              <NavDropdown title={<PersonCircle size={24} color="black" />} id="navbarScrollingDropdown">
-                <NavDropdown.Item href="/login">Accedi</NavDropdown.Item>
-                <NavDropdown.Item href="/register">Registrati</NavDropdown.Item>
+              <NavDropdown align="end" title={<PersonCircle size={24} color="black" />} id="navbar-auth-dropdown">
+                <NavDropdown.Item as={Link} to="/login" onClick={closeMenu}>
+                  Accedi
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/register" onClick={closeMenu}>
+                  Registrati
+                </NavDropdown.Item>
               </NavDropdown>
               <CartIcon />
             </>
           )}
         </div>
       </Container>
-    </nav>
+    </Navbar>
   );
 };
 
